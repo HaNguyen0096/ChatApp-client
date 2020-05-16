dd66fd520814ba333b25c3c673ddb8bc8a049001
Ha Nguyen <hanguyen.0096@gmail.com>
2020-05-15 22:16:58 -0400

    setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
+import Join from './components/Join'
+import Chat from './components/Chat'
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <Router>
+      <Route path='/' exact component={Join} />
+      <Route path='/chat' component={Chat} />
+    </Router>
   )
 }
 
