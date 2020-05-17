d479dc9042f65b345c25eb6dc312ef46783a6c28
Ha Nguyen <hanguyen.0096@gmail.com>
2020-05-16 20:20:17 -0400

    add login interface

--- src/components/Join/Join.js
+++ src/components/Join/Join.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
+import './Join.css'
 
 const Join = () => {
   const [name, setName] = useState('')
@@ -9,9 +10,9 @@ const Join = () => {
     <div className='joinOuterContainer'>
       <div className='joinInnerContainer'>
         <h1 className='heading'>Join</h1>
-        <div><input placeholder='' className='joinInput' type='text' onChange={} /></div>
-        <div><input placeholder='' className='joinInput mt-20' type='text' onChange={} /></div>
-        <Link>
+        <div><input placeholder='Name' className='joinInput' type='text' onChange={(e) => setName(e.target.value)} /></div>
+        <div><input placeholder='Room' className='joinInput mt-20' type='text' onChange={(e) => setRoom(e.target.value)} /></div>
+        <Link onClick={e => (!name || !room) ? e.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
           <button className='button mt-20' type='submit'>Sign In</button>
         </Link>
       </div>
